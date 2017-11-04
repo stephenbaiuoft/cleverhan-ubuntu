082d9e5518906a4a6660ad597c2ef4a9a9b584d6
Stephen Bai <stephenbai91@gmail.com>
2017-11-04 18:06:38 -0400

    setup_2

--- fgsm_saliency.py
+++ fgsm_saliency.py
@@ -203,7 +203,13 @@ def minist_fgsm_saliency(train_start=0, train_end=10, test_start=0,
         # correct_y_set: correct_y_output used for training
 
         adv_x_set = None
-        correct_y_set = None
+        adv_y_target = None
+
+        # we need multi x_train_saliency / y_train_saliency
+        #
+        x_train_saliency = None
+        y_train_saliency = None
+
         for sample_ind in xrange(0, source_samples):
             print('--------------------------------------')
             print('Saliency Attacking input %i/%i' % (sample_ind + 1, source_samples))
@@ -211,6 +217,17 @@ def minist_fgsm_saliency(train_start=0, train_end=10, test_start=0,
 
             current_class = int(np.argmax(Y_test[sample_ind]))
             target_classes = other_classes(nb_classes, current_class)
+
+            # Create x_train_saliency
+            if x_train_saliency is not None:
+                xtmp = np.tile(sample, source_samples - 1)
+                x_train_saliency = np.stack((x_train_saliency, xtmp))
+
+                ytmp = np.tile(Y_test[sample_ind], source_samples - 1)
+                y_train_saliency = np.stack((y_train_saliency), ytmp)
+            else:
+                x_train_saliency = np.tile(sample, source_samples - 1)
+                y_train_saliency = np.tile(Y_test[sample_ind], source_samples - 1)
 
             # Loop over all target classes
             for target in target_classes:
@@ -225,10 +242,10 @@ def minist_fgsm_saliency(train_start=0, train_end=10, test_start=0,
 
                 # Add to adv_x_set, correct_y_set
                 if adv_x_set is not None:
-                    correct_y_set = np.stack(correct_y_set, Y_test[sample_ind])
+                    adv_y_target = np.stack(adv_y_target, one_hot_target)
                     adv_x_set = np.stack((adv_x_np, adv_x_set))
                 else:
-                    correct_y_set = Y_test[sample_ind]
+                    adv_y_target = one_hot_target
                     adv_x_set = adv_x_np
 
                 # Check if success was achieved
@@ -247,14 +264,35 @@ def minist_fgsm_saliency(train_start=0, train_end=10, test_start=0,
         # here we have successfully stacked up x_adversarial_set, y_correct_set
         # these can be used to provide training to our model now
         print("Checking x_adv_set shape: ", adv_x_set.shape)
-        print("Checking correct_y_set shape: ", correct_y_set.shape)
+        print("Checking correct_y_set shape: ", adv_y_target.shape)
+
+        print("x_training_saliency shape:", x_train_saliency.shape)
+        print("y_training_saliency shape:", y_train_saliency.shape)
+
+        # now train saliency map model
+        model_saliency = make_basic_cnn(nb_filters=nb_filters)
+        preds_saliency = model_saliency(x_train_saliency)
+
+        jsma_params = {'theta': 1., 'gamma': 0.1,
+                       'clip_min': 0., 'clip_max': 1.,
+                       'y_target': adv_y_target}
+
+        # create adv_saliency set, using x_train data and jsma_params containing adv_y_target
+        adv_saliency = jsma.generate(x_train_saliency, jsma_params)
+        preds_saliency_adv = model_saliency(adv_saliency)
 
 
     # Redefine TF model FGSM!!!
     model_2 = make_basic_cnn(nb_filters=nb_filters)
     preds_2 = model_2(x)
     fgsm2 = FastGradientMethod(model_2, sess=sess)
-    adv_x_2 = fgsm2.generate(x, **fgsm_params)
+
+    # parameter for FGSM
+    fgsm_params_y = {'eps': 0.3,
+                     'y': y,
+                     'clip_min': 0.,
+                     'clip_max': 1.}
+    adv_x_2 = fgsm2.generate(x, **fgsm_params_y)
     if not backprop_through_attack:
         # For the fgsm attack used in this tutorial, the attack has zero
         # gradient so enabling this flag does not change the gradient.
@@ -301,181 +339,10 @@ def minist_fgsm_saliency(train_start=0, train_end=10, test_start=0,
     return report
 
 
-# jsma function
-def mnist_tutorial_jsma(train_start=0, train_end=60000, test_start=0,
-                        test_end=10000, viz_enabled=True, nb_epochs=6,
-                        batch_size=128, nb_classes=10, source_samples=10,
-                        learning_rate=0.001):
-    """
-    MNIST tutorial for the Jacobian-based saliency map approach (JSMA)
-    :param train_start: index of first training set example
-    :param train_end: index of last training set example
-    :param test_start: index of first test set example
-    :param test_end: index of last test set example
-    :param viz_enabled: (boolean) activate plots of adversarial examples
-    :param nb_epochs: number of epochs to train model
-    :param batch_size: size of training batches
-    :param nb_classes: number of output classes
-    :param source_samples: number of test inputs to attack
-    :param learning_rate: learning rate for training
-    :return: an AccuracyReport object
-    """
-    # Object used to keep track of (and return) key accuracies
-    report = AccuracyReport()
-
-    # MNIST-specific dimensions
-    img_rows = 28
-    img_cols = 28
-    channels = 1
-
-    # Set TF random seed to improve reproducibility
-    tf.set_random_seed(1234)
-
-    # Create TF session and set as Keras backend session
-    sess = tf.Session()
-    print("Created TensorFlow session.")
-
-    set_log_level(logging.DEBUG)
-
-    # Get MNIST test data
-    X_train, Y_train, X_test, Y_test = data_mnist(train_start=train_start,
-                                                  train_end=train_end,
-                                                  test_start=test_start,
-                                                  test_end=test_end)
-
-    # Define input TF placeholder
-    x = tf.placeholder(tf.float32, shape=(None, 28, 28, 1))
-    y = tf.placeholder(tf.float32, shape=(None, 10))
-
-    # Define TF model graph
-    model = make_basic_cnn()
-    preds = model(x)
-    print("Defined TensorFlow model graph.")
-
-    ###########################################################################
-    # Training the model using TensorFlow
-    ###########################################################################
-
-    # Train an MNIST model
-    train_params = {
-        'nb_epochs': nb_epochs,
-        'batch_size': batch_size,
-        'learning_rate': learning_rate
-    }
-    sess.run(tf.global_variables_initializer())
-    rng = np.random.RandomState([2017, 8, 30])
-    model_train(sess, x, y, preds, X_train, Y_train, args=train_params,
-                rng=rng)
-
-    # Evaluate the accuracy of the MNIST model on legitimate test examples
-    eval_params = {'batch_size': batch_size}
-    accuracy = model_eval(sess, x, y, preds, X_test, Y_test, args=eval_params)
-    assert X_test.shape[0] == test_end - test_start, X_test.shape
-    print('Test accuracy on legitimate test examples: {0}'.format(accuracy))
-    report.clean_train_clean_eval = accuracy
-
-    ###########################################################################
-    # Craft adversarial examples using the Jacobian-based saliency map approach
-    ###########################################################################
-    print('Crafting ' + str(source_samples) + ' * ' + str(nb_classes-1) +
-          ' adversarial examples')
-
-    # Keep track of success (adversarial example classified in target)
-    results = np.zeros((nb_classes, source_samples), dtype='i')
-
-    # Rate of perturbed features for each test set example and target class
-    perturbations = np.zeros((nb_classes, source_samples), dtype='f')
-
-    # Initialize our array for grid visualization
-    grid_shape = (nb_classes, nb_classes, img_rows, img_cols, channels)
-    grid_viz_data = np.zeros(grid_shape, dtype='f')
-
-    # Instantiate a SaliencyMapMethod attack object
-    jsma = SaliencyMapMethod(model, back='tf', sess=sess)
-    jsma_params = {'theta': 1., 'gamma': 0.1,
-                   'clip_min': 0., 'clip_max': 1.,
-                   'y_target': None}
-
-    figure = None
-    # Loop over the samples we want to perturb into adversarial examples
-    for sample_ind in xrange(0, source_samples):
-        print('--------------------------------------')
-        print('Attacking input %i/%i' % (sample_ind + 1, source_samples))
-        sample = X_test[sample_ind:(sample_ind+1)]
-
-        # We want to find an adversarial example for each possible target class
-        # (i.e. all classes that differ from the label given in the dataset)
-        current_class = int(np.argmax(Y_test[sample_ind]))
-        target_classes = other_classes(nb_classes, current_class)
-
-        # For the grid visualization, keep original images along the diagonal
-        grid_viz_data[current_class, current_class, :, :, :] = np.reshape(
-            sample, (img_rows, img_cols, channels))
-
-        # Loop over all target classes
-        for target in target_classes:
-            print('Generating adv. example for target class %i' % target)
-
-            # This call runs the Jacobian-based saliency map approach
-            one_hot_target = np.zeros((1, nb_classes), dtype=np.float32)
-            one_hot_target[0, target] = 1
-            jsma_params['y_target'] = one_hot_target
-            adv_x = jsma.generate_np(sample, **jsma_params)
-
-            # Check if success was achieved
-            res = int(model_argmax(sess, x, preds, adv_x) == target)
-
-            # Computer number of modified features
-            adv_x_reshape = adv_x.reshape(-1)
-            test_in_reshape = X_test[sample_ind].reshape(-1)
-            nb_changed = np.where(adv_x_reshape != test_in_reshape)[0].shape[0]
-            percent_perturb = float(nb_changed) / adv_x.reshape(-1).shape[0]
-
-            # Display the original and adversarial images side-by-side
-            if viz_enabled:
-                figure = pair_visual(
-                    np.reshape(sample, (img_rows, img_cols)),
-                    np.reshape(adv_x, (img_rows, img_cols)), figure)
-
-            # Add our adversarial example to our grid data
-            grid_viz_data[target, current_class, :, :, :] = np.reshape(
-                adv_x, (img_rows, img_cols, channels))
-
-            # Update the arrays for later analysis
-            results[target, sample_ind] = res
-            perturbations[target, sample_ind] = percent_perturb
-
-    print('--------------------------------------')
-
-    # Compute the number of adversarial examples that were successfully found
-    nb_targets_tried = ((nb_classes - 1) * source_samples)
-    succ_rate = float(np.sum(results)) / nb_targets_tried
-    print('Avg. rate of successful adv. examples {0:.4f}'.format(succ_rate))
-    report.clean_train_adv_eval = 1. - succ_rate
-
-    # Compute the average distortion introduced by the algorithm
-    percent_perturbed = np.mean(perturbations)
-    print('Avg. rate of perturbed features {0:.4f}'.format(percent_perturbed))
-
-    # Compute the average distortion introduced for successful samples only
-    percent_perturb_succ = np.mean(perturbations * (results == 1))
-    print('Avg. rate of perturbed features for successful '
-          'adversarial examples {0:.4f}'.format(percent_perturb_succ))
-
-    # Close TF session
-    sess.close()
-
-    # Finally, block & display a grid of all the adversarial examples
-    if viz_enabled:
-        import matplotlib.pyplot as plt
-        plt.close(figure)
-        _ = grid_visual(grid_viz_data)
-
-    return report
 
 
 def main(argv=None):
-    mnist_tutorial(nb_epochs=FLAGS.nb_epochs, batch_size=FLAGS.batch_size,
+    minist_fgsm_saliency(nb_epochs=FLAGS.nb_epochs, batch_size=FLAGS.batch_size,
                    learning_rate=FLAGS.learning_rate,
                    clean_train=FLAGS.clean_train,
                    backprop_through_attack=FLAGS.backprop_through_attack,
